c4aaee129ad9bf61a489fb4b2e0c38060f3cd798
Laksh18 <sahasra.m10@gmail.com>
2025-11-25 06:52:32 +0100

    Lesson 37

---
+++ Activity 2.py
@@ -0,0 +1,13 @@
+# Print a sentence
+print("Hello, welcome to the world of Python")
+
+# Print a number
+print(18)
+
+# Next line after print
+print("Hello Python \n")
+
+# Print all the keywords in Python
+print ("Keywords in Python are...")
+import keyword
+print(keyword.kwlist)
---
+++ Activity.py
@@ -0,0 +1,15 @@
+num1 = 1
+# Printing the variable
+print(num1)
+
+# Printing a greeting message
+print("Hello, World!")
+
+# Printing my name
+print("My name is Sahasra.")
+
+# Printing a fun fact about myself
+print("I like to code in Python.")
+
+# Performing and printing a simple calculation
+print("The result of 5 + 3 is:", 5 + 3)
